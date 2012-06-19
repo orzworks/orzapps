929c738e9f90bd04162c5d028f0caf6d5dc28958
J. orz <orzworks@gmail.com>
2012-06-19 00:31:43 -0400

    first commit of orzapp dinnerpicker

---
+++ dinnerpicker/dinnerpicker/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnDinner = New System.Windows.Forms.Button
+        Me.lblDinnerTitle = New System.Windows.Forms.Label
+        Me.lblDinner = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'btnDinner
+        '
+        Me.btnDinner.Location = New System.Drawing.Point(96, 111)
+        Me.btnDinner.Name = "btnDinner"
+        Me.btnDinner.Size = New System.Drawing.Size(99, 23)
+        Me.btnDinner.TabIndex = 0
+        Me.btnDinner.Text = "Pick My Dinner"
+        Me.btnDinner.UseVisualStyleBackColor = True
+        '
+        'lblDinnerTitle
+        '
+        Me.lblDinnerTitle.AutoSize = True
+        Me.lblDinnerTitle.Location = New System.Drawing.Point(111, 9)
+        Me.lblDinnerTitle.Name = "lblDinnerTitle"
+        Me.lblDinnerTitle.Size = New System.Drawing.Size(74, 13)
+        Me.lblDinnerTitle.TabIndex = 1
+        Me.lblDinnerTitle.Text = "Your dinner is:"
+        Me.lblDinnerTitle.Visible = False
+        '
+        'lblDinner
+        '
+        Me.lblDinner.AutoSize = True
+        Me.lblDinner.Location = New System.Drawing.Point(93, 53)
+        Me.lblDinner.Name = "lblDinner"
+        Me.lblDinner.Size = New System.Drawing.Size(16, 13)
+        Me.lblDinner.TabIndex = 2
+        Me.lblDinner.Text = "   "
+        Me.lblDinner.Visible = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 148)
+        Me.Controls.Add(Me.lblDinner)
+        Me.Controls.Add(Me.lblDinnerTitle)
+        Me.Controls.Add(Me.btnDinner)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.Text = "Dinner Picker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnDinner As System.Windows.Forms.Button
+    Friend WithEvents lblDinnerTitle As System.Windows.Forms.Label
+    Friend WithEvents lblDinner As System.Windows.Forms.Label
+
+End Class
---
+++ dinnerpicker/dinnerpicker/Form1.vb
@@ -0,0 +1,59 @@
+﻿'THIS PIECE OF SHIT IS RELEASED UNDER THE WTFPL
+'This program is free software. It comes without any warranty, to
+'* the extent permitted by applicable law. You can redistribute it
+'* and/or modify it under the terms of the Do What The Fuck You Want
+'* To Public License, Version 2, as published by Sam Hocevar. See
+'* http://sam.zoy.org/wtfpl/COPYING for more details.
+'....................../´¯/) 
+'....................,/¯../ 
+'.................../..../ 
+'............./´¯/'...'/´¯¯`·¸ 
+'........../'/.../..../......./¨¯\ 
+'........('(...´...´.... ¯~/'...') 
+'.........\.................'...../ 
+'..........''...\.......... _.·´ 
+'............\..............( 
+'..............\.............\...
+
+
+
+
+Public Class frmMain
+    'when clicking this button, the code uses a ranomizing function to output what the user should eat.
+    Private Sub btnDinner_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDinner.Click
+        'this the code what sets up some random number generators
+        Dim rndDinner As Random
+        Dim intDinner As Integer
+        rndDinner = New Random
+        intDinner = rndDinner.Next(1, 11)
+
+        'this the code what links a number to a phrase and sets the label on the form to the phrase
+        If intDinner = 1 Then
+            lblDinner.Text = "veggie patty"
+        ElseIf intDinner = 2 Then
+            lblDinner.Text = "Sandwich of meat"
+        ElseIf intDinner = 3 Then
+            lblDinner.Text = "sandwich of jelly"
+        ElseIf intDinner = 4 Then
+            lblDinner.Text = "Eggs and Bacon"
+        ElseIf intDinner = 5 Then
+            lblDinner.Text = "Eggs and Sausages"
+        ElseIf intDinner = 6 Then
+            lblDinner.Text = "Chicken"
+        ElseIf intDinner = 7 Then
+            lblDinner.Text = "KD"
+        ElseIf intDinner = 8 Then
+            lblDinner.Text = "Ramen"
+        ElseIf intDinner = 9 Then
+            lblDinner.Text = "Pastaaa"
+        ElseIf intDinner = 10 Then
+            lblDinner.Text = "cereal"
+        ElseIf intDinner = 11 Then
+            lblDinner.Text = "pancakes"
+        End If
+
+        'sets the labels visible
+        lblDinnerTitle.Visible = True
+        lblDinner.Visible = True
+    End Sub
+End Class
